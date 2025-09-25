000bd64a778332da5720e692d1f8a65b84bdcac6
cybersuicide <swiptenholt2001@gmail.com>
2025-09-25 11:53:04 +0200

    HMI Port

---
+++ Fresh Paintings 1.1/assets/minecraft/holdmyitems/item_addon.js
@@ -0,0 +1,20 @@
+// Cyber was here :3
+
+if(I.isOf(item, Items.get("minecraft:painting"))){
+    M.moveY(matrices, 0.025);
+    M.moveX(matrices, -0.05);
+    M.rotateX(matrices, -7.5);
+    M.rotateY(matrices, -5);
+}
+if(I.isOf(item, Items.get("minecraft:item_frame"))){
+    M.moveY(matrices, 0.025);
+    M.moveX(matrices, -0.05);
+    M.rotateX(matrices, -7.5);
+    M.rotateY(matrices, -5);
+}
+if(I.isOf(item, Items.get("minecraft:glow_item_frame"))){
+    M.moveY(matrices, 0.025);
+    M.moveX(matrices, -0.05);
+    M.rotateX(matrices, -7.5);
+    M.rotateY(matrices, -5);
+}
